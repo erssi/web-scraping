9f4e68a6639eb96ab2384621c152413dd5c7784b
ersi xhangoli <ersi@motomtech.com>
2022-06-23 11:20:34 +0200

    convert onlineShopping to api

--- README.md
+++ README.md
@@ -4,3 +4,43 @@
 # time.sleep(2)
 # windows C:/Users/User/Downloads/chromedriver
 # mac /Users/ersixhangoli/Downloads/chromedriver
+
+
+
+# online shopping
+# cd onlineShopping
+# Linux
+sudo apt-get install python3-venv    # If needed
+python3 -m venv .venv
+source .venv/bin/activate
+
+# macOS
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Windows
+py -3 -m venv .venv
+.venv\scripts\activate
+
+python -m pip install --upgrade pip
+python -m pip install flask
+
+
+
+
+
+
+
+
+
+
+
+
+
+# run app
+1- source .venv/bin/activate
+2- python -m flask run
+
+
+
+
--- onlineShopping/app.py
+++ onlineShopping/app.py
@@ -1,33 +1,84 @@
-from cgi import print_exception
+from flask import Flask
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+app = Flask(__name__)
+
+@app.route("online-shopping-scrape", methods=['POST'])
+def home():
+    return ali_expres_scrape('laptops')
 
 
-def amazon_get_url(search_term):
+def amazon_scrape(search_term):
+    
     template = 'https://amazon.com/s?k={}&sprefix=monitor%2Caps%2C178&ref=nb_sb_ss_ts-doa-p_2_7'
     search_term = search_term.replace(' ', '+')
 
     url = template.format(search_term)
 
-    return url
+    amazonRecords = []
+    driver = webdriver.Chrome('C:/Users/User/Downloads/chromedriver')
+    # url = amazon_get_url( search_term)
+    # for page in (1, 21):
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    results = soup.find_all('div', {'data-component-type': 's-search-result'})
 
-def ebay_get_url(search_term):
+    for item in results:
+        record = extract_amazon_record(item)
+        if record: 
+             amazonRecords.append(record)
+    driver.close()
+    # print(amazonRecords)
+    return amazonRecords
+
+def ebay_scrape(search_term):
     template = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw={}&_sacat=0&LH_TitleDesc=0&_odkw=monitor&_osacat=0'
     search_term = search_term.replace(' ', '+')
 
     url = template.format(search_term)
+    ebayRecords = []
+    driver = webdriver.Chrome('C:/Users/User/Downloads/chromedriver')
+    # url = ebay_get_url(search_term)
 
-    return url
+    # for page in (1, 21):
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    results = soup.find_all('div', {'class': 's-item__info clearfix'})
+    image = soup.find_all('img', {'class': 's-item__image-img'})
+    for item in range(len(results)):
+        record = extract_ebay_record(results[item], image[item])
+        if record: 
+            ebayRecords.append(record)
+    driver.close()
+    # print(ebayRecords[1])
+    return ebayRecords
 
-def ali_expres_get_url(search_term):
+def ali_expres_scrape(search_term):
+
     template ='https://www.aliexpress.com/wholesale?catId=0&initiative_id=SB_20220614113010&SearchText={}&spm=a2g0o.home.1000002.0'
 
     search_term = search_term.replace(' ', '+')
 
     url = template.format(search_term)
 
-    return url
+    aliExpresRecords = []
+    driver = webdriver.Chrome('/Users/ersixhangoli/Downloads/chromedriver')
+    # url = ali_expres_get_url(search_term)
+
+    # for page in (1, 11):
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    results = soup.find_all('a', {'class': '_3t7zg _2f4Ho'})
+       
+    for item in results:
+        record = extract_ali_expres_record(item)
+        if record: 
+            aliExpresRecords.append(record)
+    driver.close()
+
+    return aliExpresRecords
+
 
 
 def extract_amazon_record(item):
@@ -143,58 +194,4 @@ def extract_ali_expres_record(item):
     
     return {'description': description, 'image': image, 'price': price, 'rating': rating, 'url': url}
 
-def amazon_scrape(search_term):
-    amazonRecords = []
-    driver = webdriver.Chrome('C:/Users/User/Downloads/chromedriver')
-    url = amazon_get_url( search_term)
-    for page in (1, 21):
-        driver.get(url.format(page))
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        results = soup.find_all('div', {'data-component-type': 's-search-result'})
 
-    for item in results:
-        record = extract_amazon_record(item)
-        if record: 
-             amazonRecords.append(record)
-    driver.close()
-    print(amazonRecords)
-
-def ebay_scrape(search_term):
-    ebayRecords = []
-    driver = webdriver.Chrome('C:/Users/User/Downloads/chromedriver')
-    url = ebay_get_url(search_term)
-
-
-    for page in (1, 21):
-        driver.get(url.format(page))
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        results = soup.find_all('div', {'class': 's-item__info clearfix'})
-        image = soup.find_all('img', {'class': 's-item__image-img'})
-    for item in range(len(results)):
-        record = extract_ebay_record(results[item], image[item])
-        if record: 
-            ebayRecords.append(record)
-    driver.close()
-    print(ebayRecords[1])
-
-def ali_expres_scrape(search_term):
-    aliExpresRecords = []
-    driver = webdriver.Chrome('C:/Users/User/Downloads/chromedriver')
-    url = ali_expres_get_url(search_term)
-
-    for page in (1, 11):
-        driver.get(url.format(page))
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        results = soup.find_all('a', {'class': '_3t7zg _2f4Ho'})
-       
-    for item in results:
-        record = extract_ali_expres_record(item)
-        if record: 
-            aliExpresRecords.append(record)
-    driver.close()
-
-
-
-# amazon_scrape('laptops')
-# ebay_scrape('laptops')
-ali_expres_scrape('monitor arm')
